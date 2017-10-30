e5b7b078b34663a975bb50f91dc152dd603d73e1
hellsdark <hellsdark@gmail.com>
2017-10-30 23:17:39 +0100

    fix paths

--- mastermind/mastermind.js
+++ mastermind/mastermind.js
@@ -2,12 +2,13 @@ var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var path = require('path');
 
 app.get('/', function(req, res){
-    res.sendFile('/home/pi/web/mastermind/mastermind.html');
+    res.sendFile(path.resolve(__dirname,'.')+'/mastermind.html');
 });
 
-app.use(express.static('/home/pi/web/mastermind'));
+app.use(express.static(path.resolve(__dirname,'.')));
 
 var player1=false;
 var player2=false;
